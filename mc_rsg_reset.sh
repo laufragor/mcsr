869f1b135a0f390b1fc1a7789657c42115bf19fd
laufragor <masterthunderman@gmail.com>
2021-06-22 17:20:53 +0200

    Update mc_rsg_reset.sh

--- mc_rsg_reset.sh
+++ mc_rsg_reset.sh
@@ -1,29 +1,37 @@
 #!/usr/bin/env bash
-#
-# reset.sh by neoprene1337
-# small edits by lauf#6785
+#     by Lauf#3696 - original version by neoprene1337
 
-delay="0.050"
+# 1. This is 70ms delay at the end of each world creation screen - it's very safe.
+#     You can lower it - just make sure that *each screen* of world creation gets shown in your recording
+
+# 2. Adjust main_menu_wait according to how fast your pc quits the world
+
+delay="0.070"  
+main_menu_wait="2.0" 
+
 
 xdotool version &>/dev/null || { echo >&2 "xdotool not installed!"; exit 1; }
-wid="$(xdotool search --sync --onlyvisible --name Minecraft)"
-[ -z "$wid" ] && { echo >&2 "failed to retrieve minecraft window!"; exit 1; }
+wid="$(xdotool getwindowfocus)"
+[[ "$(xdotool getwindowname $wid)" = Minecraft* ]] || exit
 
-# sleep a bit to avoid F8 ghosting with xdotool inputs
-sleep 0.3
-xdotool key --window "$wid" Tab; sleep "$delay"
-xdotool key --window "$wid" Return; sleep "$delay"
+sleep 0.2    # to avoid ghosting between the key you use for this script, and xdotool inputs
+xdotool key --window "$wid" Escape
+xdotool key --window "$wid" Shift+Tab
+xdotool key --window "$wid" Return
+sleep $main_menu_wait      # wait for main menu
+xdotool key --window "$wid" Tab;
+xdotool key --window "$wid" Return;
+xdotool key --window "$wid" Shift+Tab;
 xdotool key --window "$wid" Shift+Tab; sleep "$delay"
+xdotool key --window "$wid" Return;
+# easy difficulty + create
+xdotool key --window "$wid" Tab;
+xdotool key --window "$wid" Tab;
+xdotool key --window "$wid" Return;
+xdotool key --window "$wid" Return;
+xdotool key --window "$wid" Return;
+xdotool key --window "$wid" Shift+Tab;
+xdotool key --window "$wid" Shift+Tab;
+xdotool key --window "$wid" Shift+Tab;
 xdotool key --window "$wid" Shift+Tab; sleep "$delay"
-xdotool key --window "$wid" Return; sleep "$delay"
-# easy difficulty
-xdotool key --window "$wid" Tab; sleep "$delay"
-xdotool key --window "$wid" Tab; sleep "$delay"
-xdotool key --window "$wid" Return; sleep "$delay"
-xdotool key --window "$wid" Return; sleep "$delay"
-xdotool key --window "$wid" Return; sleep "$delay"
-xdotool key --window "$wid" Shift+Tab; sleep "$delay"
-xdotool key --window "$wid" Shift+Tab; sleep "$delay"
-xdotool key --window "$wid" Shift+Tab; sleep "$delay"
-xdotool key --window "$wid" Shift+Tab; sleep "$delay"
-xdotool key --window "$wid" Return; sleep "$delay"
+xdotool key --window "$wid" Return; 
