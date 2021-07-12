f4abbec9b806bbffe595a77bf1eba5cf4065ed3c
laufragor <masterthunderman@gmail.com>
2021-07-13 01:33:31 +0200

    Update mc_rsg_reset.sh

--- mc_rsg_reset.sh
+++ mc_rsg_reset.sh
@@ -1,37 +1,40 @@
 #!/usr/bin/env bash
-#     by Lauf#3696 - original version by neoprene1337
+#     made by Lauf#3696 - original version by neoprene1337
 
-# 1. This is 70ms delay at the end of each world creation screen - it's very safe.
-#     You can lower it - just make sure that *each screen* of world creation gets shown in your recording
+# 1. Record yourself using this script, and make sure that *each world creation screen* gets shown in the recording.
+#     play with the delay if that doesn't happen, and try again
 
-# 2. Adjust main_menu_wait according to how fast your pc quits the world
-
-delay="0.070"  
-main_menu_wait="2.0" 
-
+delay="0.040"  
 
 xdotool version &>/dev/null || { echo >&2 "xdotool not installed!"; exit 1; }
 wid="$(xdotool getwindowfocus)"
 [[ "$(xdotool getwindowname $wid)" = Minecraft* ]] || exit
 
-sleep 0.2    # to avoid ghosting between the key you use for this script, and xdotool inputs
-xdotool key --window "$wid" Escape
-xdotool key --window "$wid" Shift+Tab
-xdotool key --window "$wid" Return
-sleep $main_menu_wait      # wait for main menu
-xdotool key --window "$wid" Tab;
-xdotool key --window "$wid" Return;
-xdotool key --window "$wid" Shift+Tab;
-xdotool key --window "$wid" Shift+Tab; sleep "$delay"
-xdotool key --window "$wid" Return;
-# easy difficulty + create
-xdotool key --window "$wid" Tab;
-xdotool key --window "$wid" Tab;
-xdotool key --window "$wid" Return;
-xdotool key --window "$wid" Return;
-xdotool key --window "$wid" Return;
-xdotool key --window "$wid" Shift+Tab;
-xdotool key --window "$wid" Shift+Tab;
-xdotool key --window "$wid" Shift+Tab;
-xdotool key --window "$wid" Shift+Tab; sleep "$delay"
-xdotool key --window "$wid" Return; 
+# quit world
+sleep 0.2  
+xdotool windowactivate --sync "$wid" key Escape
+xdotool windowactivate --sync "$wid" key Shift+Tab
+xdotool windowactivate --sync "$wid" key Return
+
+# wait for main menu
+while [[ "$(xdotool getwindowname $wid)" = *player ]]; do
+  :    # wait
+done
+sleep 0.5
+
+# create world
+xdotool windowactivate --sync "$wid" key Tab;
+xdotool windowactivate --sync "$wid" key Return;
+xdotool windowactivate --sync "$wid" key Shift+Tab;
+xdotool windowactivate --sync "$wid" key Shift+Tab; sleep "$delay"
+xdotool windowactivate --sync "$wid" key Return;
+xdotool windowactivate --sync "$wid" key Tab;
+xdotool windowactivate --sync "$wid" key Tab;
+xdotool windowactivate --sync "$wid" key Return;
+xdotool windowactivate --sync "$wid" key Return;
+xdotool windowactivate --sync "$wid" key Return;
+xdotool windowactivate --sync "$wid" key Shift+Tab;
+xdotool windowactivate --sync "$wid" key Shift+Tab;
+xdotool windowactivate --sync "$wid" key Shift+Tab;
+xdotool windowactivate --sync "$wid" key Shift+Tab; sleep "$delay"
+xdotool windowactivate --sync "$wid" key Return; 
